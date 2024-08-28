9220534d7e3ac65382a91894d6bfc56aec6161ce
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-08-27 23:06:34 -0700

    Update App with simple State based on onChange and onClick events.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = e => {
+    setSubmitted(false);
+    setEnteredPlayerName(e.target.value);
+  }
+
+  const handleClick = () => setSubmitted(true);
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
