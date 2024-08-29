090ca8f16ffd44991d8368d6ddc34ed423724987
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-08-29 00:54:57 -0700

    Created ResultModal component to appear when timerExpired is true in order to display whether the user won or lost and how they did.

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from 'react';
+import ResultModal from './ResultModal.jsx';
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
@@ -16,18 +17,20 @@ export default function TimerChallenge({ title, targetTime }) {
   };
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>You lost</p>}
-      <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : null}</p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-          {timerStarted ? 'Stop' : 'Start'} Challenge
-        </button>
-      </p>
-      <p className={timerStarted ? 'active' : undefined}>
-        {timerStarted ? 'Time is running...' : 'Timer inactive'}
-      </p>
-    </section>
+    <>
+      {timerExpired && <ResultModal result="lost" targetTime={targetTime}/>}
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : null}</p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? 'active' : undefined}>
+          {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        </p>
+      </section>
+    </>
   );
 }
