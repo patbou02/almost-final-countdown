f5405a68bbcc30dc60f1464e1ca36b239d08653c
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-08-30 00:33:17 -0700

    Added a score to be displayed instead of the "you lost" message.

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -6,6 +6,7 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -18,6 +19,7 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
   return (
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
       <p>The target time was <strong>{targetTime}</strong> seconds.</p>
       <p>You stopped the timer with <strong>{formattedRemainingTime} seconds</strong> left.</p>
       <form method="dialog" onSubmit={onReset}>
