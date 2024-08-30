25ff3d7d7e4828e00f4852faabacff1b94e50b34
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-08-29 23:24:41 -0700

    Used the React hook forwardRef within the ResultModal component to allow for ref to be passed down from TimerChallenge. This way the built-in showModal() method can be called within the handleStart method. Also removed condition to show/hide modal since modal will not be visible anyways unless showModal() gets called.

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -3,12 +3,16 @@ import ResultModal from './ResultModal.jsx';
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
+  const dialog = useRef();
 
   const [ timerStarted, setTimerStarted ] = useState(false);
   const [ timerExpired, setTimerExpired ] = useState(false);
 
   const handleStart = () => {
-    timer.current = setTimeout(() => setTimerExpired(true), targetTime * 1000);
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      dialog.current.showModal();
+    }, targetTime * 1000);
     setTimerStarted(true);
   };
 
@@ -18,7 +22,7 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      {timerExpired && <ResultModal result="lost" targetTime={targetTime}/>}
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : null}</p>
